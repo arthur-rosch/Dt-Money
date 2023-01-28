ab3f97d0c2def007a9983d815b70e87e31cd3a38
Arthur Rosch <rosch.arthur@gmail.com>
2023-01-28 01:48:31 -0300

    Projeto Finalizado

--- src/hooks/useSummary.tsx
+++ src/hooks/useSummary.tsx
@@ -1,4 +1,5 @@
 import { useContextSelector } from 'use-context-selector'
+import { useMemo } from 'react'
 import { TransactionsContext } from '../contexts/TransactionsContext'
 
 export function useSummary() {
@@ -6,23 +7,24 @@ export function useSummary() {
     return context.transactions
   })
 
-  const summary = transactions.reduce(
-    (acc, transactions) => {
-      if (transactions.type === 'income') {
-        acc.income += transactions.price
-        acc.total += transactions.price
-      } else {
-        acc.outcome += transactions.price
-        acc.total -= transactions.price
-      }
-      return acc
-    },
-    {
-      income: 0,
-      outcome: 0,
-      total: 0,
-    },
-  )
-
+  const summary = useMemo(() => {
+    transactions.reduce(
+      (acc, transactions) => {
+        if (transactions.type === 'income') {
+          acc.income += transactions.price
+          acc.total += transactions.price
+        } else {
+          acc.outcome += transactions.price
+          acc.total -= transactions.price
+        }
+        return acc
+      },
+      {
+        income: 0,
+        outcome: 0,
+        total: 0,
+      },
+    )
+  }, [transactions])
   return summary
 }
